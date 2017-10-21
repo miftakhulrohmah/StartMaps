766169ee666a8a105d93d7a5ab44762865e37f3a
Miftahul Rohmah <miftahul_rohmah_24rpl@student.smktelkom-mlg.sch.id>
2017-10-22 00:34:27 +0700

    Adding API

--- app/src/main/java/id/sch/smktelkom_mlg/startmaps/MainActivity.java
+++ app/src/main/java/id/sch/smktelkom_mlg/startmaps/MainActivity.java
@@ -2,12 +2,47 @@ package id.sch.smktelkom_mlg.startmaps;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.TextView;
 
-public class MainActivity extends AppCompatActivity {
+import com.google.android.gms.common.ConnectionResult;
+import com.google.android.gms.common.api.GoogleApiClient;
+import com.google.android.gms.location.LocationRequest;
+import com.google.android.gms.location.LocationServices;
+
+public class MainActivity extends AppCompatActivity implements GoogleApiClient.ConnectionCallbacks, GoogleApiClient.OnConnectionFailedListener {
+
+
+    private final String LOG_TAG = "LaurenceTestApp";
+     private TextView txtOutput;
+     private GoogleApiClient mGoogleApiClient;
+     private LocationRequest mLocationRequest;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+                        mGoogleApiClient = new GoogleApiClient.Builder(this)
+                                .addApi(LocationServices.API)
+                                .addConnectionCallbacks(this)
+                                .addOnConnectionFailedListener(this)
+                                .build();
+
+                        txtOutput = (TextView) findViewById(R.id.txtOutput);
+    }
+
+    @Override
+    public void onConnected(Bundle bundle) {
+
+    }
+
+    @Override
+    public void onConnectionSuspended(int i) {
+
+    }
+
+    @Override
+    public void onConnectionFailed(ConnectionResult connectionResult) {
+
     }
 }
